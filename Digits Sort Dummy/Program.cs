43281076429b19466d5abc325e8d37ae52ddcaa9
Ramon Rinaldi <rarin_8@hotmail.com>
2023-08-07 08:56:13 +0200

    Dummy project created

---
+++ Digits Sort Dummy/Program.cs
@@ -0,0 +1,19 @@
+﻿// Functions
+int ReadInt()
+{
+    int i;
+    do
+    {
+        string s = Console.ReadLine();
+        int.TryParse(s, out i);
+    } while (i <= 0);
+    return i;
+}
+// TODO: implement your functions
+
+
+// Program
+Console.WriteLine("Enter an integer value bigger 0");
+int inputValue = ReadInt();
+Console.WriteLine("You entered {0}", inputValue);
+// TODO: implement your program
