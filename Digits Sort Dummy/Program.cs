6c0d63aac6c767bf6574290c2c55d5b46f396791
CL04\Ramon.Rinaldi <rarin_8@hotmail.com>
2023-08-14 08:16:24 +0200

    Dont use top level statement

--- Digits Sort Dummy/Program.cs
+++ Digits Sort Dummy/Program.cs
@@ -1,19 +1,26 @@
-﻿// Functions
-int ReadInt()
+﻿internal class Program
 {
-    int i;
-    do
+    private static void Main(string[] args)
     {
-        string s = Console.ReadLine();
-        int.TryParse(s, out i);
-    } while (i <= 0);
-    return i;
+        Console.WriteLine("Enter an integer value bigger 0");
+        int inputValue = ReadInt();
+        Console.WriteLine("You entered {0}", inputValue);
+
+        // TODO: implement your program
+
+    }
+
+    static int ReadInt()
+    {
+        int i;
+        do
+        {
+            string s = Console.ReadLine();
+            int.TryParse(s, out i);
+        } while (i <= 0);
+        return i;
+    }
+
+    // TODO: implement your functions
+
 }
-// TODO: implement your functions
-
-
-// Program
-Console.WriteLine("Enter an integer value bigger 0");
-int inputValue = ReadInt();
-Console.WriteLine("You entered {0}", inputValue);
-// TODO: implement your program
